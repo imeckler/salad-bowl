5645dfc79d3914d5a251a6394af4e651af35cc70
Izaak Meckler <ihmeckler@gmail.com>
2020-03-21 15:01:57 -0700

    working

--- salad_bowl/salad_bowl.ml
+++ salad_bowl/salad_bowl.ml
@@ -7,6 +7,7 @@ type error =
   | Game_finished
   | Wrong_player_id
   | Invalid_transition
+[@@deriving sexp]
 
 exception E of error
 
@@ -23,20 +24,40 @@ module Round = struct
     | One_word -> raise (E Game_finished)
 end
 
+module Time = struct
+  include Sexpable.Of_sexpable(String)(struct
+      type t = Time.t
+      let to_sexpable = Time.to_string
+      let of_sexpable = Time.of_string
+      end)
+  include Time
+end
+
 let subround_length = Time.Span.of_min 2.
 
+module Pair(F: sig
+    type _ t [@@deriving bin_io, sexp]
+    val map: 'a t -> f:('a -> 'b) -> 'b t
+  end) = struct
+    type 'a t = 'a F.t * 'a F.t
+    [@@deriving bin_io, sexp]
+    let map (a, b) ~f = (F.map ~f a, F.map ~f b)
+end
+
 module Game = struct
-  module Teams = struct
-    type t = Player_id.t list * Player_id.t list
-  [@@deriving bin_io]
-  end
+  module Teams = Pair(List)
 
   module Team_state = struct
-    type t =
-      { players: Player_id.t array
+    type 'a t =
+      { players: 'a array
       ; active: int
       }
     [@@deriving sexp, bin_io]
+
+    let map t ~f =
+      { t with
+        players=Array.map t.players ~f
+      }
 
     let create players =
       { players= Array.of_list players
@@ -47,13 +68,15 @@ module Game = struct
       { t with active= (t.active + 1) mod Array.length t.players }
   end
 
+  module Team_states = Pair(Team_state)
+
   let next_team = function
     | `A -> `B
     | `B -> `A
 
   module Words = struct
     type t = {completed: string list; remaining : string list }
-    [@@deriving bin_io]
+    [@@deriving bin_io, sexp]
 
     let create ws =
         {completed= [] ; remaining = List.permute ws }
@@ -70,24 +93,43 @@ module Game = struct
   end
 
   module Running = struct
+    module Team_states = struct
+      type 'a t =
+        { teams: 'a Team_states.t; active: [ `A | `B ] }
+      [@@deriving bin_io, sexp]
+
+      let map t ~f =
+        { t with teams= Team_states.map ~f t.teams }
+
+      let active_player { active; teams=(a,b) } =
+        let team =
+          match active with
+          | `A -> a
+          | `B -> b
+        in
+        team.players.(team.active)
+
+      let advance { active; teams=(a,b) } =
+        let (active, teams) =
+          match active with
+          | `A ->
+            ( `B, (a, Team_state.advance b) )
+          | `B ->
+            ( `A, (Team_state.advance a, b))
+        in
+        { active; teams }
+    end
+
     type t =
-        { teams: Team_state.t * Team_state.t
-        ; active_team : [ `A | `B]
+        { teams: Player_id.t Team_states.t
         ; subround_start: Time.t option
         ; round: Round.t
         ; words: Words.t
         }
-      [@@deriving bin_io]
+      [@@deriving bin_io, sexp]
 
     let active_player t =
-      let team = t.active_team in
-      let a, b = t.teams in
-      let team =
-        match team with
-        | `A -> a
-        | `B -> b
-      in
-      team.players.(team.active)
+      Team_states.active_player t.teams
 
     let current_subround_over t ~now:now0 =
       List.is_empty t.words.remaining ||
@@ -97,24 +139,17 @@ module Game = struct
         Time.(Span.(diff now0 time >= subround_length))
 
     let advance t =
-      let (a, b) = t.teams in
-      let (active_team, teams) =
-        match t.active_team with
-        | `A ->
-          ( `B, (a, Team_state.advance b) )
-        | `B ->
-          ( `A, (Team_state.advance a, b))
-      in
-      { t with active_team; teams }
+      { t with teams= Team_states.advance t.teams }
   end
 
   type t =
-    | Players_joining of Player_id.Set.t
-    | Collecting_words of Teams.t * string list Player_id.Map.t
+    | Players_joining of { players: Player_id.Set.t; admin: Player_id.t }
+    | Collecting_words of { teams: Player_id.t Teams.t ; words: string list Player_id.Map.t; admin: Player_id.t }
     | Running of Running.t
-  [@@deriving bin_io]
+  [@@deriving bin_io, sexp]
 
-  let init = Players_joining Player_id.Set.empty
+  let init admin =
+    Players_joining { players=Player_id.Set.empty; admin }
 
   module Update = struct
     (* All updates will come with the player so we check if it's valid *)
@@ -127,26 +162,35 @@ module Game = struct
 
       | Start_subround
       | Next_word
-  [@@deriving bin_io]
+  [@@deriving bin_io, sexp]
   end
 
   let update_exn ~now ~player (game : t) (u : Update.t) =
     match game, u with
-    | Players_joining players, Join ->
-      Players_joining (Set.add players player)
-    | Players_joining players, Finalize_players ->
-      let n = Set.length players in
-      let teams = List.split_n (Set.to_list players) n in
-      Collecting_words (teams, Player_id.Map.empty)
-    | Collecting_words (teams, words), Add_word w ->
+    | Players_joining r, Join ->
+      Players_joining { r with players=Set.add r.players player }
+    | Players_joining { players; admin }, Finalize_players ->
+      printf "%s :)\n%!" __LOC__ ;
+      if Player_id.equal player admin
+      then
+        let n = Set.length players / 2 in
+        let teams = List.split_n (Set.to_list players) n in
+        Collecting_words {admin; teams;words= Player_id.Map.empty}
+      else
+        game
+    | Collecting_words r, Add_word w ->
       Collecting_words
-        (teams, Map.update words player ~f:(function
+        { r with
+          words= Map.update r.words player ~f:(function
              | None -> [ w ]
-             | Some ws -> w :: ws ))
-    | Collecting_words ((a, b), words), Finalize_words ->
+             | Some ws -> w :: ws )
+        }
+    | Collecting_words { teams=(a, b); words; admin=_ }, Finalize_words ->
       Running
-        { teams= Team_state.(create a, create b)
-        ; active_team= `A
+        { teams=
+            { teams= Team_state.(create a, create b)
+            ; active= `A
+            }
         ; subround_start= None
         ; words=
           Words.create (
@@ -167,11 +211,9 @@ module Game = struct
         let g =
           match g.subround_start with
           | None -> g
-          | Some _ ->
-            { (Running.advance g) with
-              subround_start= Some now;
-            }
+          | Some _ -> Running.advance g
         in
+        let g = { g with subround_start= Some now } in
         (
         let on_deck_player = Running.active_player g in
           if not Player_id.(on_deck_player = player)
@@ -184,7 +226,9 @@ module Game = struct
         | None -> game
         | Some ws -> Running { g with words = ws }
         end
-    | _ -> raise (E Invalid_transition)
+    | _ ->
+      printf ":0 oh no %s\n%!" __LOC__ ;
+      raise (E Invalid_transition)
 
   let update ~now ~player game u =
     try Ok (update_exn ~now ~player game u)
@@ -192,39 +236,53 @@ module Game = struct
 
   module View = struct
     type state =
-      | Players_joining
-      | Collecting_words of Teams.t
+      | Players_joining of { players: string list; admin: bool }
+      | Collecting_words of { teams: string Teams.t; words: string list; admin: bool }
       | Running of
           { round: Round.t
-          ; teams: Team_state.t * Team_state.t
+          ; teams: (Player_id.t * string) Running.Team_states.t
           ; subround_start: Time.t option
-          ; active_team: [`A | `B]
-          ; current_word: string option }
+          ; current_word: [`Out_of_words | `Not_your_turn | `Word of string ] }
       | Finished
-    [@@deriving bin_io]
+    [@@deriving bin_io, sexp]
 
     type t =
       { game: Game_id.t
       ; state: state
       }
-    [@@deriving bin_io]
+    [@@deriving bin_io, sexp]
   end
 
-  let view player game : View.state =
+  let view registered_players player game : View.state =
     match game with
-    | Players_joining _ -> Players_joining
-    | Collecting_words (ts, _) -> Collecting_words ts
-    | Running ({ round; teams; active_team; subround_start; words } as r )->
+    | Players_joining { players=ps; admin } ->
+      Players_joining
+        { players=List.map (Set.to_list ps)
+              ~f:(Map.find_exn registered_players) 
+        ; admin= Player_id.equal player admin
+        }
+    | Collecting_words { teams=ts; words=wss; admin } ->
+      let ws =
+        match Map.find wss player with
+        | Some ws -> ws
+        | None -> []
+      in
+      let teams = Teams.map ts ~f:(Map.find_exn registered_players) in
+      Collecting_words {teams; words= ws; admin= Player_id.equal player admin }
+    | Running ({ round; teams; subround_start; words } as r )->
       let current_word =
-        if Player_id.equal (Running.active_player r) player
-        then
-          List.hd words.remaining
-        else None
+        match List.hd words.remaining with
+        | None -> `Out_of_words
+        | Some w ->
+          if Player_id.equal (Running.active_player r) player
+          then `Word w
+          else `Not_your_turn
       in
       Running
         { round
-        ; teams
-        ; active_team
+        ; teams=
+            Running.Team_states.map teams
+              ~f:(fun p -> (p, Map.find_exn registered_players p))
         ; subround_start
         ; current_word 
         }
@@ -246,7 +304,7 @@ module Rpcs = struct
     type query = 
       | Create_game of Player_id.t
       | Join_game of Player_id.t * Game_id.t
-    [@@deriving bin_io]
+    [@@deriving bin_io, sexp]
 
     let t =
       Rpc.State_rpc.create
--- server/server.ml
+++ server/server.ml
@@ -5,13 +5,15 @@ open Async
 module App_state = struct
   type per_game = 
     { game      : Game.t
-    ; listeners : (Player_id.t * Game.View.state Pipe.Writer.t) list
+    ; listeners : (Player_id.t * Game.View.state Pipe.Writer.t sexp_opaque) list
     }
+  [@@deriving sexp]
 
   type t =
     { games : per_game Game_id.Map.t
     ; players: string Player_id.Map.t
     }
+  [@@deriving sexp]
 
   let init =
     { games = Game_id.Map.empty
@@ -30,7 +32,11 @@ let initiate (state : App_state.t) (q : Rpcs.Initiate.query) =
                 Game_id.Map.set state.games
                   ~key:game
                   ~data:{
-                    game=Players_joining (Player_id.Set.singleton player);
+                    game=
+                      Players_joining
+                        { players=Player_id.Set.singleton player
+                        ; admin= player
+                        };
                     listeners=[(player, w)]
                   }
             }
@@ -40,7 +46,7 @@ let initiate (state : App_state.t) (q : Rpcs.Initiate.query) =
             { state with games=
               Map.update state.games game ~f:(function
                 | Some {game=Players_joining g; listeners } ->
-                  { game= Players_joining (Set.add g player)
+                  { game= Players_joining { g with players= Set.add g.players player }
                   ; listeners= (player, w) :: listeners
                   }
                 | _ -> return (Or_error.error_string "Game not joinable" )
@@ -55,54 +61,136 @@ let register (state : App_state.t) (q : string) =
   let id = Player_id.create () in
   ({ state with players=Map.set state.players ~key:id ~data:q }, id)
 
+let port = 8001
+
 let main () =
-  let state =
-    ref App_state.init
+  let get_state, set_state =
+    let r = ref App_state.init in
+    ( (fun () -> !r),
+      (fun s ->
+(*          Core.printf !"state: %{sexp: App_state.t}\n%!" s ; *)
+         r := s) )
   in
-  Tcp.Server.create
-    (Tcp.Where_to_listen.of_port 8000)
-    ~on_handler_error:`Raise
-    (fun _ r w -> 
-       Async.Rpc.Connection.serve_with_transport
-         ~handshake_timeout:(Some (Time.Span.of_sec 10.))
-         ~heartbeat_config:None
-         ~description:(Info.of_string "foo")
-         ~connection_state:(fun _ -> ())
-         ~on_handshake_error:`Raise
-         ~implementations:(
-           Rpc.Implementations.create_exn
-             ~on_unknown_rpc:`Raise
-             ~implementations:
-               [ Rpc.Rpc.implement Rpcs.Register.t
-                   (fun () q -> 
-                      let s, resp = register !state q in
-                      state := s ;
-                      Deferred.return resp )
-               ; Rpc.State_rpc.implement Rpcs.Initiate.t
-                   (fun () q ->
-                      let open Or_error.Let_syntax in
-                      Deferred.return begin
-                        let%map (s, _p, g, r) = initiate !state q in
-                        state := s;
-                        ({ Game.View.game=g; state=Players_joining }, r ) 
-                      end
-                   )
-               ; Rpc.One_way.implement Rpcs.Move.t 
-                   (fun () (player, game_id, u) ->
-                      match Map.find !state.games game_id with
-                      | None -> ()
-                      | Some { game; listeners } ->
-                        match 
-                          Game.update ~now:(Time.now()) ~player 
-                            game u
-                        with
-                        | Error _e -> ()
-                        | Ok game ->
-                          state :=
-                            { !state with games= Map.set !state.games ~key:game_id ~data:{ game; listeners } } ;
-                          List.iter listeners ~f:(fun (p, w) ->
-                              Pipe.write_without_pushback w
-                                (Game.view p game)) )
-               ] )
-         (Rpc.Transport.of_reader_writer r w
-            ~max_message_size:1_000_000) )
+  let%bind _ =
+    Tcp.Server.create
+      (Tcp.Where_to_listen.of_port port)
+      ~on_handler_error:(`Call (fun _addr exn ->
+          eprintf "handler_error: %s" (Exn.to_string exn) ))
+      (fun _ sock_r sock_w -> 
+         let to_client_r, to_client_w = Pipe.create () in
+         let from_client_r, from_client_w = Pipe.create () in
+
+         (*
+         let ws_r, ws_w = Pipe.cre
+         let bytes_r, bytes_w = Pipe.create () in
+            *)
+         let _stop = 
+          Websocket_async.server
+            ~check_request:(fun _req ->
+                Core.printf "hi%!\n%!";
+                return true )
+            ~reader:sock_r ~writer:sock_w
+            ~app_to_ws:to_client_r
+            ~ws_to_app:from_client_w
+            ()
+         in
+         let from_client_r =
+           let from_client_r1, w1 = Pipe.create () in
+           don't_wait_for (
+           Pipe.iter_without_pushback from_client_r ~f:(fun x ->
+               Pipe.write_without_pushback w1 x )  ) ;
+           from_client_r1
+         in
+         let%bind transport =
+           let f t =
+             match t.Websocket.Frame.opcode with
+             | Text | Binary -> Some t.content
+             | _ -> None
+           in
+           let%bind r =
+             Pipe.filter_map from_client_r ~f
+            |> Reader.of_pipe (Info.of_string "")
+           in
+           let%map (w, _) =
+             let str_r, str_w = Pipe.create () in
+             don't_wait_for (
+               Pipe.transfer str_r to_client_w
+                 ~f:(fun s ->
+                     Websocket.Frame.create ~opcode:Binary ~content:s ()) ) ;
+             Writer.of_pipe (Info.of_string "") str_w
+           in
+           Rpc.Transport.of_reader_writer ~max_message_size:10_000 r w
+        in
+        Async.Rpc.Connection.serve_with_transport
+          ~handshake_timeout:(Some (Time.Span.of_sec 10.))
+          ~heartbeat_config:None
+          ~description:(Info.of_string "foo")
+          ~connection_state:(fun _ -> ())
+          ~on_handshake_error:(`Call(fun exn ->
+          eprintf "handler_error: %s\n%!" (Exn.to_string exn) ))
+          ~implementations:(
+            Rpc.Implementations.create_exn
+              ~on_unknown_rpc:(`Call (fun () ~rpc_tag ~version ->
+                  eprintf "unknown_rpc: %s, %d\n%!" rpc_tag version;
+                `Continue))
+              ~implementations:
+                [ Rpc.Rpc.implement Rpcs.Register.t
+                    (fun () q -> 
+                        let s, resp = register (get_state()) q in
+                        set_state s ;
+                        Deferred.return resp )
+                ; Rpc.State_rpc.implement Rpcs.Initiate.t
+                    (fun () q ->
+                        let open Or_error.Let_syntax in
+                        Deferred.return begin
+                          let%map (s, p, g, r) = initiate (get_state ()) q in
+                          set_state s;
+
+                          let {App_state.game;listeners} = Map.find_exn s.games g in
+
+                          List.iter listeners ~f:(fun (p', w) ->
+                              if Player_id.(p <> p') then
+                              Pipe.write_without_pushback_if_open w
+                                (Game.view s.players p' game) );
+
+                          ({ Game.View.game=g
+                           ; state=Game.view s.players p game
+                           }, r ) 
+                        end
+                    )
+                ; Rpc.One_way.implement Rpcs.Move.t 
+                    (fun () (player, game_id, u) ->
+                        match Map.find (get_state ()).games game_id with
+                        | None -> ()
+                        | Some { game; listeners } ->
+                          Core.printf !"Move: %{sexp:Game.Update.t}\n%!" u ;
+                          Core.printf !"before: %{sexp:Game.t}\n%!" game ;
+                          match 
+                            Game.update ~now:(Time.now()) ~player 
+                              game u
+                          with
+                          | Error e -> 
+                            eprintf !"move %s %{sexp:error}\n%!" __LOC__  e ;
+                            ()
+                          | Ok game ->
+                            Core.printf !"after: %{sexp:Game.t}\n%!" game ;
+                            set_state
+                              { (get_state ()) with games= Map.set (get_state ()).games ~key:game_id ~data:{ game; listeners } } ;
+                            List.iter listeners ~f:(fun (p, w) ->
+                                let view = Game.view (get_state ()).players p game in
+                                Pipe.write_without_pushback_if_open w
+                                  view ;
+                              ) )
+                ] )
+          transport
+      )
+  in
+  never ()
+
+let () = 
+  Command.async
+    ~summary:"server"
+    Command.Let_syntax.(
+      return main
+    )
+  |> Command.run
